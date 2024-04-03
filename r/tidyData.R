8df704fb89c82e2324bd081d07998d41789cc4af
Júlio R <102775547+Serkanovic@users.noreply.github.com>
2024-04-03 10:28:11 -0300

    data analysis with r

---
+++ r/tidyData.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(palmerpenguins)
+library(ggthemes)
+
+View(penguins)
+#?penguins
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) +
+  geom_point(mapping = aes(color = species, shape = species)) +
+  geom_smooth(method = "lm") +
+  labs(
+    title = "Body mass and flipper lenght",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+    x = "Flipper length (mm)", y = "Body mass (g)",
+    color = "Species", shape = "Species"
+  ) +
+  scale_color_colorblind()
