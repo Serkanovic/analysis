1f46ff6b90014cbe39ae0cb27dbfdcca17a4c515
Júlio R <102775547+Serkanovic@users.noreply.github.com>
2024-04-09 09:37:17 -0300

    c lang

---
+++ lang/c/adv.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    printf("Jogo de adivinhacao\n");
+
+    //int a = 42;
+    //printf("Numero: %d\n", a);
+    int valor;
+
+    printf("Digite um valor: ");
+    scanf("%d",&valor);
+    printf("Valor inserido: %d\n",valor);
+
+}
