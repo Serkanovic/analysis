eba5827e3063fc6daf7ad869ff390f2ce1dcbed1
Júlio R <102775547+Serkanovic@users.noreply.github.com>
2024-04-12 09:22:48 -0300

    file updated

--- lang/c/adv.c
+++ lang/c/adv.c
@@ -2,13 +2,28 @@
 
 int main(){
     printf("Jogo de adivinhacao\n");
-
-    //int a = 42;
+    int a = 55;
+    int tentativas = 3;
     //printf("Numero: %d\n", a);
     int valor;
 
-    printf("Digite um valor: ");
-    scanf("%d",&valor);
-    printf("Valor inserido: %d\n",valor);
+    for(int i = 1; i <=tentativas; i++){
+        printf("Tentativa %d de %d\n",i,tentativas);
+        printf("Digite um valor: ");
+        scanf("%d",&valor);
+        printf("Valor inserido: %d\n",valor);
 
+        if(valor == a){
+            printf("Valor correto.\n");
+            break;
+        }
+        else{
+            if(valor > a){
+                printf("O valor foi maior que o escolhido\n");
+            }
+            else{
+                printf("O valor foi menor que o escolhido\n");
+            }
+        }
+    }
 }
