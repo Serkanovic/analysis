2716047d33009061848ceba6dc3ea3f9820589b3
Júlio R <102775547+Serkanovic@users.noreply.github.com>
2024-07-11 14:21:53 -0300

    Update projPy.py

--- python/e-commerce/projPy.py
+++ python/e-commerce/projPy.py
@@ -5,10 +5,22 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import scipy
+from scipy.stats import normaltest
+from scipy.stats import chi2_contingency
+
 #carregamento dos dados
 df = pd.read_csv("dados.csv")
 
 print(df.head)
+
+#Valores ausentes ou duplibacos
+dup = df["product_id"].duplicated().sum()
+aus = df.isna().sum()
+print("Valores duplicados: \n")
+print(dup)
+print("Valores ausentes: \n")
+print(aus)
 
 print("Linhas e colunas" + df.shape)
 print(df["category"].values_counts())
@@ -21,3 +33,13 @@ plt.show()
 
 sns.barplot(data = df, x  = "price", y = "category", color = "seagreen")
 plt.show()
+
+#Teste de normalidade
+numericalColumns = ['price', 'review_score']
+
+for i in numericalColumns:
+    stats, pval = normaltest(df[i])
+    if pval > 0.05:
+        print(i, ': Segue uma distribuição normal')
+    else:
+        print(i, ': Não segue uma distribuição normal')
